ab9ce2c61f5a7fac0877cc169a696aaf21d82a9a
alymurtazamemon <alimurtaza-assistant@outlook.com>
2022-12-29 19:39:09 +0500

    add Netspec inside Escrow Contracts.

--- backend/contracts/Escrow.sol
+++ backend/contracts/Escrow.sol
@@ -1,15 +1,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
+/**
+ * @title Escrow Smart Contract.
+ * @author EscrowFactory Smart Contract.
+ * @notice This is the contract that will handle the addresses of beneficiary and arbiter as well as funds that after approval will be transfered to beneficiary.
+ * @dev This contract will be created through EscrowFactory Smart Contract.
+ */
 contract Escrow {
     // * arbiter will be the owner of the contract.
+
+    /**
+     * @notice arbiter will be the address that will approve the agreement.
+     */
     address public arbiter;
+    /**
+     * @notice beneficiary will be the address that will receive the funds after approval.
+     */
     address public beneficiary;
+    /**
+     * @notice depositor will be the address that will deposit the funds after approval.
+     */
     address public depositor;
+    /**
+     * @notice This will be the boolean value which will let the caller know about the approval state of this contract.
+     */
     bool public isApproved;
 
+    /**
+     * @notice This event will be emitted and contain the balance which will be transfered to beneficiary after approval by the arbiter of this contract.
+     */
     event Approved(uint);
 
+    /**
+     * @dev This constructor will take the addresses of depositor, beneficiary and arbiter as well as it will take the msg.value which will be deposited to beneficiary address later after approval of arbiter.
+     * @param _depositor address.
+     * @param _beneficiary address.
+     * @param _arbiter address.
+     */
     constructor(
         address _depositor,
         address _beneficiary,
@@ -20,6 +48,9 @@ contract Escrow {
         arbiter = _arbiter;
     }
 
+    /**
+     * @notice This function will transfer the funds to beneficiary address after approval of arbiter and emit an Approval event.
+     */
     function approve() external {
         uint balance = address(this).balance;
         (bool sent, ) = payable(beneficiary).call{value: balance}("");
@@ -28,6 +59,10 @@ contract Escrow {
         isApproved = true;
     }
 
+    /**
+     * @notice This function will return the balance of the contract.
+     * @return uint value in wei.
+     */
     function getBalance() external view returns (uint) {
         return address(this).balance;
     }
